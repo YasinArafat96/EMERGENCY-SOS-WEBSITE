097a501e40fee8c129d9adc413503b39030ca544
samiyaoishee <samiya.rahman.oishee@g.bracu.ac.bd>
2026-09-01 19:12:00 +0600

    update user.js

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -9,14 +9,36 @@ const {
   getPrimaryHelpers,
   updateLocation,
   getNearbyHelpers,
+  sendFriendRequest,
+  getFriendRequests,
+  getSentFriendRequests,
+  acceptFriendRequest,
+  rejectFriendRequest,
+  getFriends,
+  removeFriend,
+  uploadAvatar,
 } = require('../controllers/userController');
+
+const { uploadSingle, handleUploadError } = require('../middleware/upload');
 
 router.get('/profile', protect, getProfile);
 router.put('/profile', protect, updateProfile);
-router.get('/:userId', protect, getUserById);
 router.post('/primary-helper', protect, addPrimaryHelper);
 router.get('/primary-helpers', protect, getPrimaryHelpers);
 router.put('/location', protect, updateLocation);
 router.get('/nearby-helpers', protect, getNearbyHelpers);
+
+router.post('/friend-request', protect, sendFriendRequest);
+router.get('/friend-requests', protect, getFriendRequests);
+router.get('/sent-friend-requests', protect, getSentFriendRequests);
+router.put('/friend-request/:requestId/accept', protect, acceptFriendRequest);
+router.put('/friend-request/:requestId/reject', protect, rejectFriendRequest);
+router.get('/friends', protect, getFriends);
+router.delete('/friends/:friendId', protect, removeFriend);
+
+router.get('/:userId', protect, getUserById);
+
+// Upload avatar
+router.post('/profile/avatar', protect, uploadSingle, handleUploadError, uploadAvatar);
 
 module.exports = router;
